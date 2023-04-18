4a090b11f3bdb47c692eca67d9737ca312dfd5a5
236660 Strachota Piotr <236660@edu.p.lodz.pl>
2023-04-19 01:22:54 +0200

    enable TLS

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: fs.readFileSync('key.pem'),
+      cert: fs.readFileSync('certificate.pem'),
+    },
+  });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Product Store API')
+    .setVersion('1.0')
+    .addTag('products')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+  await app.listen(443);
 }
+
 bootstrap();
